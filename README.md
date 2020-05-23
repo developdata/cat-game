d44924b5e3c3804448649a95d5c4fbb96947ffdb
Glitch (game-cat) <none>
2020-05-23 09:32:17 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# cat-game
+# cat ga,e
+
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+// our default array of dreams
+const dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/dreams", (request, response) => {
+  // express helps us take JS objects and send them as JSON
+  response.json(dreams);
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
